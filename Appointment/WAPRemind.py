833bbbc2693de68685182c793cfb94bf9967ee34
lanway <115564098@qq.com>
2016-11-20 23:47:16 +0800

    修复计算3天bug

--- Appointment/WAPRemind.py
+++ Appointment/WAPRemind.py
@@ -21,7 +21,7 @@ class WAPRemind(BaseHandler):
         for WAP in WAPs:
             delta = datetime.datetime.now() - WAP.WAPcreateT
             #判断是否已经创建了3天
-            if delta>datetime.timedelta(day=3):
+            if delta>datetime.timedelta(days=3):
                 # 是否选择了用户，未选择则发送提醒短信
                 # chooser = self.db.query(WApInfo).filter(WApInfo.WAIappoid == WAP.WAPid,WApInfo.WAIvalid == 1).all()
                 # if not chooser:
