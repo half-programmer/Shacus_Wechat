bb8ffa516a80c9e487ebac26f8c705c76b11c823
half-programmer <Kingxc96@outlook.com>
2016-11-21 16:21:20 +0800

    增加登录时判断是否有形象墙

--- Userinfo/WUserjudge.py
+++ Userinfo/WUserjudge.py
@@ -7,20 +7,28 @@
 import json
 
 from BaseHandlerh import BaseHandler
-from Database.tables import User
+from Database.tables import User, Homepageimage
 
 
 class WUserjudge(BaseHandler):
 
 
-    retjson = {'code': '', 'contents': ''}
+    retjson = {'code': '', 'contents': '','upic':''}
     def get(self):
 
         u_id = self.get_argument('id')
         phone = self.get_argument("phone")
 
         try:
-            exist = self.db.query(User).filter(User.Uid == u_id,User.Utel == phone).one()
+            exist = self.db.query(User).filter(User.Uid == u_id, User.Utel == phone).one()
+            # 判断形象墙是否有图片
+            try:
+                hp_imgs = self.db.query(Homepageimage).filter(Homepageimage.HPuser == exist.Uid).one()
+                if hp_imgs:
+                    self.retjson['upic'] = 1
+            except Exception, e:
+                self.retjson['upic'] = 0
+                print e
             self.retjson['code'] = '10500'
             self.retjson['contents'] = '验证成功'
         except Exception,e:
