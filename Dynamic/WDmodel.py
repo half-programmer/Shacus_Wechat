b8a40ae62535a0e7c68f16b2debdbf8d4e910dae
lanway <115564098@qq.com>
2016-11-26 23:55:52 +0800

    shijian

--- Dynamic/WDmodel.py
+++ Dynamic/WDmodel.py
@@ -92,7 +92,7 @@ class WDmodel(object):
                 did=wd.WDid,
                 dimgs=pic_urls,
                 dimgs_abb=pic_urls_abb,
-                dtime=wd.WDcreateT.strftime('%Y-%m-%d %h:%m'),
+                dtime=wd.WDcreateT.strftime('%Y-%m-%d %H:%M'),
             )
             return ret_ap
         except Exception, e:
